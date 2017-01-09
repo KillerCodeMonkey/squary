cd8f009b72b1bbaae205fa5f1ea7293dfb837562
Bengt Weiße <bengtler@gmail.com>
2017-01-09 17:03:29 +0100

    initial commit

---
+++ src/levels.js
@@ -0,0 +1,49 @@
+export default [{
+    name: '1',
+    blockSize: 20,
+    start: {
+        x: 20,
+        y: 20 * 7
+    },
+    map: [
+        'xxxxxxxxxxxxxxxxxxxxxxxxx',
+        'xxxxxxx                 x',
+        'xx   o                 xx',
+        'x                o     xx',
+        'x                      xx',
+        'x                x     xx',
+        'x                x     xx',
+        'x                x     xx',
+        'xxxxxxxxxxxxxxxxxo     xx',
+        'xxxxxxxxxxxxxxxxxxxxxxxxx'
+    ]
+}, {
+    name: '2',
+    blockSize: 10,
+    start: {
+        x: 10,
+        y: 10 * 14
+    },
+    map: [
+        'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        'xxxxxxx                                          x',
+        'xx                                              xx',
+        'x                                               xx',
+        'x                                               xx',
+        'x                   o                           xx',
+        'x                                               xx',
+        'x                                               xx',
+        'x                                               xx',
+        'x                                               xx',
+        'x                                               xx',
+        'x                             o                 xx',
+        'x                                               xx',
+        'x                                               xx',
+        'x                                               xx',
+        'x        o       x                              xx',
+        'x                x                              xx',
+        'x                x                              xx',
+        'xxxxxxxxxxxxxxxxx                               xx',
+        'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    ]
+}]
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import 'pixi';
+import 'p2';
+import Phaser from 'phaser';
+
+import GameState from './states/Game';
+import MenuState from './states/Menu';
+
+class Game extends Phaser.Game {
+
+  constructor () {
+    super(500, 200, Phaser.AUTO, 'content', null);
+
+    this.state.add('Menu', MenuState, false);
+
+    this.state.start('Menu');
+  }
+}
+
+window.game = new Game();
